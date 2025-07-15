ea9530762e8e721cf54acf9c3c8fb0a818d1f8c1
Le Nhat Huy <huylnhe186534@fpt.edu.vn>
2025-07-15 13:07:19 +0700

    APP + Backend

---
+++ Backend/EateryReviewWebsiteFE/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EateryReviewWebsiteFE.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public BlogController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            ViewBag.GoogleMapsApiKey = _configuration["GoogleMapAPI:Key"];
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.GoogleMapsApiKey = _configuration["GoogleMapAPI:Key"];
+            return View();
+        }
+    }
+}
---
+++ Backend/EateryReviewWebsiteFE/Controllers/ModerationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EateryReviewWebsiteFE.Controllers
+{
+    public class ModerationController : Controller
+    {
+        public IActionResult Review()
+        {
+            return View();
+        }
+    }
+}
