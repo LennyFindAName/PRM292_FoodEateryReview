fb247c2b168d8ca5652f69a7f15ce0a2e8b5b4b3
Le Hoang Anh (K18 HL) <anhlhhe186102@fpt.edu.vn>
2025-07-17 10:35:39 +0700

    Used the actual newest code instead of the old one.

--- Backend/EateryReviewWebsiteFE/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EateryReviewWebsiteFE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
